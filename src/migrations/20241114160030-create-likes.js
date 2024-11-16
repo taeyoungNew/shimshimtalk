a8908b2909b9026d3f83c2fda2e5563d595e6812
TAEYEONG-TOMATO <tomato.202409.minty@gmail.com>
2024-11-17 00:34:18 +0900

    accociate

---
+++ src/migrations/20241114160030-create-likes.js
@@ -0,0 +1,41 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Likes", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.UUID,
+        references: {
+          key: "id",
+          model: "Users",
+        },
+      },
+      boardId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          key: "id",
+          model: "Boards",
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Likes");
+  },
+};
